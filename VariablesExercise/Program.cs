a4e9853fd260d51901d63b362459fc90adc570a5
Rhea Epperson <re.epperson2@gmail.com>
2022-12-27 16:01:43 -0500

    finished exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // string firstName; //declration
+            //firstName = "Rhea"; //  initilization
+            string firstName = "Rhea";// declaration and initilization
+            // forumla is (datatype, varialbe name, put equal symbol, add variable value, if it's a string put variable value in qoute marks, finish with ;
+
+            // int myAge; // declaration
+            // myAge = 99; // initilization
+            int myAge = 99; // declare and init
+
+            // char firstInitial; // declaration syntax
+            // firstInitial = 'R'; // initilization
+            char firstInitial = 'R'; // declared and initiliazed
+
+            //bool isMarried; // declaration
+            //isMarried = false;  initilize
+            bool isMarried = false;
+
+            double height = 99.9;
+
+            decimal weight = 129.9m;
+
+            Console.WriteLine($"My name  is {firstName}. I am {myAge} years old (obviously I am super young). " +
+                $"My first initial is {firstInitial}. It is {isMarried} that I am married."); 
         }
     }
 }
+
+
+
+
+
